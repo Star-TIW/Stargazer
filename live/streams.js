0d6f7de7b34d96dd81c90ac6fba80bf578f9188d
Star <125516384+Star-TIW@users.noreply.github.com>
2025-09-21 09:29:03 -0700

    Update streams.js

--- live/streams.js
+++ live/streams.js
@@ -1,6 +1,16 @@
 window.gameStreams = {
-    "1234568": "https://r.dofustream.cloud/s3/cf/s3_401772731_ru5_r2_espn2/index.txt", // Test game
-    // "401772731": "https://yourlinkhere.com", 
-    // "401772732": "https://yourlinkhere.com",
-    // "GAME_ID": "STREAM_URL",
+    "401772731": "https://r.dofustream.cloud/s3/cf/s3_401772731_qlg_r2_nflnet/index.txt", // Cincinnati Bengals vs Minnesota Vikings
+    "401772732": "", // Pittsburgh Steelers vs New England Patriots
+    "401772733": "", // Indianapolis Colts vs Tennessee Titans
+    "401772734": "", // Houston Texans vs Jacksonville Jaguars
+    "401772735": "", // Denver Broncos vs Los Angeles Chargers
+    "401772736": "", // New Orleans Saints vs Seattle Seahawks
+    "401772838": "", // Atlanta Falcons vs Carolina Panthers
+    "401772839": "", // Los Angeles Rams vs Philadelphia Eagles
+    "401772840": "", // New York Jets vs Tampa Bay Buccaneers
+    "401772841": "", // Las Vegas Raiders vs Washington Commanders
+    "401772842": "", // Green Bay Packers vs Cleveland Browns
+    "401772843": "", // Arizona Cardinals vs San Francisco 49ers
+    "401772844": "", // Dallas Cowboys vs Chicago Bears
+    "401772920": "", // Kansas City Chiefs vs New York Giants
 };
