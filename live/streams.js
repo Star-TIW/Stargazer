7155f151b5e2990deb9e656fbd21496565df0722
Star <125516384+Star-TIW@users.noreply.github.com>
2025-09-21 17:21:29 -0700

    Update streams.js

--- live/streams.js
+++ live/streams.js
@@ -1,16 +1,3 @@
 window.gameStreams = {
-    "401772731": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772731_gbj_Bengals/index.txt", // Cincinnati Bengals vs Minnesota Vikings
-    "401772732": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772732_y0o_Steelers/index.txt", // Pittsburgh Steelers vs New England Patriots
-    "401772733": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772733_e4x_Colts/index.txt", // Indianapolis Colts vs Tennessee Titans
-    "401772734": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772734_nen_Texans/index.txt", // Houston Texans vs Jacksonville Jaguars
-    "401772735": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772735_zf2_Broncos/index.txt", // Denver Broncos vs Los Angeles Chargers
-    "401772736": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772736_572_Saints/index.txt", // New Orleans Saints vs Seattle Seahawks
-    "401772838": "https://s3.us-east-1.amazonaws.com/oscaar.lohmiller/panthers.m3u8", // Atlanta Falcons vs Carolina Panthers
-    "401772839": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772839_byp_Rams/index.txt", // Los Angeles Rams vs Philadelphia Eagles
-    "401772840": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772840_jec_Jets/index.txt", // New York Jets vs Tampa Bay Buccaneers
-    "401772841": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772841_skg_Raiders/index.txt", // Las Vegas Raiders vs Washington Commanders
-    "401772842": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772842_a2a_Packers/index.txt", // Green Bay Packers vs Cleveland Browns
-    "401772843": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772843_vjk_Cardinals/index.txt", // Arizona Cardinals vs San Francisco 49ers
-    "401772844": "https://dofustream.cloud/player2?m3u8=https://s3.us-east-1.amazonaws.com/oscaar.lohmiller/redzone.m3u8", // Dallas Cowboys vs Chicago Bears
-    "401772920": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772920_ei1_Chiefs/index.txt, // Kansas City Chiefs vs New York Giants
+    "401772920": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772920_ei1_Chiefs/index.txt", // Kansas City Chiefs vs New York Giants
 };
