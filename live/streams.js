198aae2db81dfb9d42fd45dbebf50209d2a70c97
Star <125516384+Star-TIW@users.noreply.github.com>
2025-09-22 13:07:42 -0700

    Update streams.js

--- live/streams.js
+++ live/streams.js
@@ -1,3 +1,3 @@
 window.gameStreams = {
-    "401772812": "https://dofustream.cloud/player?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772632_est_r2_espn2/index.txt", // Detroit Lions vs Baltimore Ravens
+    "401772812": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772632_eh3_r2_espn/index.txt, // Detroit Lions vs Baltimore Ravens
 };
