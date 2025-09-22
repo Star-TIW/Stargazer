56b55d3eb8d50c996ac4b0a539f6194a490b33c6
Star <125516384+Star-TIW@users.noreply.github.com>
2025-09-22 16:11:03 +0000

    Update streams.js

--- live/streams.js
+++ live/streams.js
@@ -1,3 +1,3 @@
 window.gameStreams = {
-    "401772920": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772920_ei1_Chiefs/index.txt", // Kansas City Chiefs vs New York Giants
+    "401772812": "https://dofustream.cloud/player?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772632_est_r2_espn2/index.txt", // Detroit Lions vs Baltimore Ravens
 };
