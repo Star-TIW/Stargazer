abef12d1921a97e6e3611d3a532c3c922244f902
Star <125516384+Star-TIW@users.noreply.github.com>
2025-09-25 20:26:34 -0700

    Update live/streams.js from HTML tool

--- live/streams.js
+++ live/streams.js
@@ -1,3 +1,2 @@
 window.gameStreams = {
-    "401772938": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772938_y9b_Seahawks/index.txt", // Seattle Seahawks vs Arizona Cardinals
 };
