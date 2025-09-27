02246328a42c25192d4ad5a95fedf76841e3df67
Star <125516384+Star-TIW@users.noreply.github.com>
2025-09-27 01:19:02 -0700

    Update live/streams.js from HTML tool

--- live/streams.js
+++ live/streams.js
@@ -1,2 +1,3 @@
 window.gameStreams = {
+    "401772632": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772632_z02_r2_fs1/index.txt", // Minnesota Vikings vs Pittsburgh Steelers
 };
