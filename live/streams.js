75e5b9931817faaa58ae8d2c0edd7f1957a5a907
Star <125516384+Star-TIW@users.noreply.github.com>
2025-09-28 12:01:22 -0700

    Update live/streams.js from HTML tool

--- live/streams.js
+++ live/streams.js
@@ -1,7 +1,7 @@
 window.gameStreams = {
     "401772737": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772737_98x_Chargers/index.txt", // Los Angeles Chargers vs New York Giants
     "401772738": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772738_etr_Titans/index.txt", // Tennessee Titans vs Houston Texans
-    "401772739": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772739_dyk_Commanders/index.txt", // Washington Commanders vs Atlanta Falcons
+    "401772739": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772739_7r9_Commanders/index.txt", // Washington Commanders vs Atlanta Falcons
     "401772740": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772740_e05_Saints/index.txt", // New Orleans Saints vs Buffalo Bills
     "401772845": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/aws/s3_401772845_1an_Eagles/index.m3u8", // Philadelphia Eagles vs Tampa Bay Buccaneers
     "401772846": "https://dofustream.cloud/player2?m3u8=https://r2.dofucdn1.store/fm2ymx.txt&type=html", // Cleveland Browns vs Detroit Lions
