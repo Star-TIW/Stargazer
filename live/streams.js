e8764a3e9c17cd4895b39e216776896952b064cf
Star <125516384+Star-TIW@users.noreply.github.com>
2025-09-29 16:14:46 -0700

    Update live/streams.js from HTML tool

--- live/streams.js
+++ live/streams.js
@@ -1,2 +1,3 @@
 window.gameStreams = {
+    "401772813": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772813_k2f_Jets/index.txt", // New York Jets vs Miami Dolphins
 };
