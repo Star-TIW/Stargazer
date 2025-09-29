28ed8386e98d5435d057096b88bae894caee15a6
Star <125516384+Star-TIW@users.noreply.github.com>
2025-09-28 17:21:52 -0700

    Update live/streams.js from HTML tool

--- live/streams.js
+++ live/streams.js
@@ -1,6 +1,3 @@
 window.gameStreams = {
-    "401772741": "https://dofustream.cloud/player2?m3u8=https://r2.dofucdn1.store/b9p4ua.txt&type=html", // Baltimore Ravens vs Kansas City Chiefs
-    "401772742": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/aws/s3_401772742_8hp_Bears/index.m3u8", // Chicago Bears vs Las Vegas Raiders
-    "401772848": "https://dofustream.cloud/player2?m3u8=https://r2.dofucdn1.store/yh6jd7.txt&type=html", // Jacksonville Jaguars vs San Francisco 49ers
-    "401772849": "https://dofustream.cloud/player2?m3u8=https://r2.dofucdn1.store/vaaem4.txt&type=html", // Indianapolis Colts vs Los Angeles Rams
+    "401772632": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772921_fc2_Packers/index.txt", // Minnesota Vikings vs Pittsburgh Steelers
 };
