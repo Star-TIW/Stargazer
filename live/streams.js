8ee3cccd82368e1ab13ac6d19a834d82f82f91d7
Star <125516384+Star-TIW@users.noreply.github.com>
2025-09-29 07:43:06 -0700

    Update streams.js

--- live/streams.js
+++ live/streams.js
@@ -1,3 +1,2 @@
 window.gameStreams = {
-    "401772921": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772921_fc2_Packers/index.txt", // Green Bay Packers vs Dallas Cowboys
 };
