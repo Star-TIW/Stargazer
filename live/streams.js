750dd64be978c50f9060565081abd310ba761480
Star <125516384+Star-TIW@users.noreply.github.com>
2025-09-29 20:25:57 -0700

    Update live/streams.js from HTML tool

--- live/streams.js
+++ live/streams.js
@@ -1,4 +1,2 @@
 window.gameStreams = {
-    "401772716": "https://dofustream.cloud/player2?m3u8=https://r2.dofucdn1.store/288rra.txt&type=html", // Cincinnati Bengals vs Denver Broncos
-    "401772813": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772813_k2f_Jets/index.txt", // New York Jets vs Miami Dolphins
 };
