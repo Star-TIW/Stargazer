dab0492931e054a4a3bc8f20fc40595f195c44fe
Star <125516384+Star-TIW@users.noreply.github.com>
2025-10-02 17:05:15 -0700

    Update live/streams.js from HTML tool

--- live/streams.js
+++ live/streams.js
@@ -1,2 +1,3 @@
 window.gameStreams = {
+    "401772939": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772939_dks_49ers/index.txt", // San Francisco 49ers vs Los Angeles Rams
 };
