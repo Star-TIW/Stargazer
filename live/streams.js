091693e8e47a20ff508eb0fde303fd36fd72e4af
Star <125516384+Star-TIW@users.noreply.github.com>
2025-10-05 09:55:00 -0700

    Update live/streams.js from HTML tool

--- live/streams.js
+++ live/streams.js
@@ -1,3 +1,9 @@
 window.gameStreams = {
     "401772633": "https://dofustream.cloud/player2?m3u8=https://r2.dofucdn1.store/akxcim.txt&type=html", // Minnesota Vikings vs Cleveland Browns
+    "401772743": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772743_nqu_Texans/index.txt", // Houston Texans vs Baltimore Ravens
+    "401772744": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772744_cwp_Giants/index.txt", // New York Giants vs New Orleans Saints
+    "401772745": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772745_6pa_Broncos/index.txt", // Denver Broncos vs Philadelphia Eagles
+    "401772850": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772850_kab_Cowboys/index.txt", // Dallas Cowboys vs New York Jets
+    "401772851": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772851_zrj_Raiders/index.txt", // Las Vegas Raiders vs Indianapolis Colts
+    "401772852": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772852_rva_Dolphins/index.txt", // Miami Dolphins vs Carolina Panthers
 };
