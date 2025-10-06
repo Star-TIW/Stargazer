c3b8186e505c8f67f779d0f2b029b77f63ea7187
Star <125516384+Star-TIW@users.noreply.github.com>
2025-10-05 23:33:14 -0700

    Update live/streams.js from HTML tool

--- live/streams.js
+++ live/streams.js
@@ -1,14 +1,2 @@
 window.gameStreams = {
-    "401772633": "Early 6 30 game", // Minnesota Vikings vs Cleveland Browns
-    "401772743": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772743_nqu_Texans/index.txt", // Houston Texans vs Baltimore Ravens
-    "401772744": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772744_cwp_Giants/index.txt", // New York Giants vs New Orleans Saints
-    "401772745": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772745_6pa_Broncos/index.txt", // Denver Broncos vs Philadelphia Eagles
-    "401772746": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772746_0k9_Buccaneers/index.txt", // Tampa Bay Buccaneers vs Seattle Seahawks
-    "401772747": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772747_v0r_Titans/index.txt", // Tennessee Titans vs Arizona Cardinals
-    "401772850": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772850_kab_Cowboys/index.txt", // Dallas Cowboys vs New York Jets
-    "401772851": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772851_zrj_Raiders/index.txt", // Las Vegas Raiders vs Indianapolis Colts
-    "401772852": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772852_rva_Dolphins/index.txt", // Miami Dolphins vs Carolina Panthers
-    "401772853": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/aws/s3_401772853_4oj_Commanders/index.m3u8", // Washington Commanders vs Los Angeles Chargers
-    "401772854": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772854_5q2_Lions/index.txt", // Detroit Lions vs Cincinnati Bengals
-    "401772922": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772922_ckz_Patriots/index.txt", // New England Patriots vs Buffalo Bills
 };
