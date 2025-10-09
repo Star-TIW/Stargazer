ee1415e666fa20f2ed43a02d8ec67bfd717de825
Star <125516384+Star-TIW@users.noreply.github.com>
2025-10-09 16:10:40 -0700

    Update live/streams.js from HTML tool

--- live/streams.js
+++ live/streams.js
@@ -1,3 +1,4 @@
 window.gameStreams = {
     "401772814": "https://dofustream.cloud/player2?m3u8=https://r.dofustream.cloud/s3/cf/s3_401772814_qnh_Chiefs/index.txt", // Kansas City Chiefs vs Jacksonville Jaguars
+    "401772940": "https://embedsports.top/embed/admin/ppv-philadelphia-eagles-vs-new-york-giants/1", // Philadelphia Eagles vs New York Giants
 };
